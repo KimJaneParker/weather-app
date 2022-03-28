ac66ac3dbeab34a3f68a59283ab1b70252b34569
KimJaneParker <kimjane.parker@gmail.com>
2022-03-28 13:29:56 +0200

    uploading weather project

---
+++ src/index.js
@@ -0,0 +1,68 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Firday",
+    "Saturday",
+  ];
+
+  return `${days[day]} ${hours}:${minutes}`;
+}
+let dateToday = document.querySelector("#dateTime");
+let currentTime = new Date();
+dateToday.innerHTML = formatDate(currentTime);
+
+function displayWeather(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  search(city);
+}
+
+function search(city) {
+  let apiKey = "d9d53cca51f57e70b244280a0f45fc6e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function location(position) {
+  let apiKey = "d9d53cca51f57e70b244280a0f45fc6e";
+  let apiUrl = `https://api.openweather.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function currentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(location);
+}
+
+let getCurrentLocation = document.querySelector("#current-location");
+getCurrentLocation.addEventListener("click", currentLocation);
+
+let searchform = document.querySelector("#search-form");
+searchform.addEventListener("submit", handleSubmit);
+
+search("Cape Town");
