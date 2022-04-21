f6caafa83d3765c021dd7e4d737f61eafa1966c5
KimJaneParker <kimjane.parker@gmail.com>
2022-04-21 12:50:20 +0200

    fixed line 30 by closing the quotation and class bracket

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ dateToday.innerHTML = formatDate(currentTime);
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class = "row`;
+  let forecastHTML = `<div class = "row">`;
   let days = ["Thu", "Fri", "Sat", "Sun"];
   days.forEach(function (day) {
     forecastHTML =
